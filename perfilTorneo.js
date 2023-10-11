ddf297a7c01d1dd4a175476010e6177c35537a81
Martin Anton <119125486+martinxanton@users.noreply.github.com>
2023-10-11 13:21:43 -0500

    Add PerfilTorneo

---
+++ perfilTorneo.js
@@ -0,0 +1,11 @@
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+    })
+}
+
+
